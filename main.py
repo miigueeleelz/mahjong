19a6cd69a90d7dae64d233b3c10249a9d10e02e2
Miguel Elizondo <miguelaea015@gmail.com>
2020-04-13 23:42:11 -0700

    added functions to create the game and print the board

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
+import constants
+import numpy
+
+board = list()
+
+
+def create_game():
+    for _ in range(2):
+        array = range(1, 6)
+        numpy.random.shuffle(array)
+        board.append(array)
+
+        array = range(6, 11)
+        numpy.random.shuffle(array)
+        board.append(array)
+
+    numpy.random.shuffle(board)
+
+
+def print_board():
+    current_element = 1
+
+    for i in range(4):
+        for j in range(5):
+            print(str(current_element) + ')' + str(board[i][j]) + '\t'),
+            current_element += 1
+        print('\n')
+
+
 if __name__ == "__main__":
-    print("Bienvenido al juego de Mahjong")
+    create_game()
+    print_board()
